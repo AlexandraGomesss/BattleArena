551d36be52d5f7d428c280bc6a43599de50d715f
AlexaG <gomes.marisa.alexa@gmail.com>
2025-03-27 18:27:36 +0000

    added character movement and Enter to continue each turn

--- src/Arena.java
+++ src/Arena.java
@@ -1,36 +1,48 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Arena {
 
     private Random random = new Random();
+    private Scanner scanner = new Scanner(System.in);
 
-    //battle begins
+    // Battle begins
     public void hostFights(Creature hero, Creature villain) {
         System.out.println("Battle begins!");
         announceFighters(hero, villain);
 
-        while(!hero.isDead() && !villain.isDead()) {
+        while (!hero.isDead() && !villain.isDead()) {
+            System.out.println("\nPress Enter to continue...");
+            scanner.nextLine(); // Wait for user to press Enter
+
             if (random.nextBoolean()) {
                 hero.takeTurn(villain);
-                if (!villain.isDead()){
+                if (!villain.isDead()) {
+                    System.out.println("\nPress Enter for " +  villain.getName() + "'s turn...");
+                    scanner.nextLine(); // Wait for Enter again
                     villain.takeTurn(hero);
                 }
             } else {
                 villain.takeTurn(hero);
-                if (!hero.isDead()) hero.takeTurn(villain);
+                if (!hero.isDead()) {
+                    System.out.println("\nPress Enter for " + hero.getName() + "'s turn...");
+                    scanner.nextLine();
+                    hero.takeTurn(villain);
+                }
             }
         }
         announceWinner(hero, villain);
     }
+
     public void announceFighters(Creature hero, Creature villain) {
-        System.out.println("Pay attention, our contenders entering the arena! " + hero.getName() + " vs " + villain.getName() + "!");
+        System.out.println("Pay attention, our contenders are entering the arena! " + hero.getName() + " vs " + villain.getName() + "!");
     }
-    private void announceWinner (Creature hero, Creature villain) {
-       if(!hero.isDead()) {
-           System.out.println("The winner is " + hero.getName() + " and the loser is " + villain.getName() + "!");
-       }
-       else {
-           System.out.println("The winner is " + villain.getName() + " and the loser is " + hero.getName() + "!");
-       }
+
+    private void announceWinner(Creature hero, Creature villain) {
+        if (!hero.isDead()) {
+            System.out.println("The winner is " + hero.getName() + " and the loser is " + villain.getName() + "!");
+        } else {
+            System.out.println("The winner is " + villain.getName() + " and the loser is " + hero.getName() + "!");
+        }
     }
 }
--- src/MySquare.java
+++ src/MySquare.java
@@ -1,6 +1,7 @@
 import org.academiadecodigo.simplegraphics.graphics.Color;
 import org.academiadecodigo.simplegraphics.graphics.Text;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
+import java.util.Random;
 
 public class MySquare {
 
@@ -9,6 +10,7 @@ public class MySquare {
     private Picture rightPlayer;
     private Text leftPlayerName;
     private Text rightPlayerName;
+    private Random random = new Random();
 
     public MySquare(int x, int y, int cellSize, String leftName, String rightName) {
         // Load the background
@@ -24,14 +26,59 @@ public class MySquare {
         rightPlayer.draw();
 
         // Display left player's name above their head
-        leftPlayerName = new Text(x + 110, y + cellSize / 4 - 20, leftName); // Adjust position
+        leftPlayerName = new Text(x + 110, y + cellSize / 4 - 20, leftName);
         leftPlayerName.setColor(Color.WHITE);
         leftPlayerName.draw();
 
-
         // Display right player's name above their head
-        rightPlayerName = new Text(x + cellSize - cellSize / 3.4, y + cellSize / 4 - 20, rightName); // Adjust position
+        rightPlayerName = new Text(x + cellSize - cellSize / 3.4, y + cellSize / 4 - 20, rightName);
         rightPlayerName.setColor(Color.WHITE);
         rightPlayerName.draw();
+
+        // Start character movement animation
+        startRandomAnimation();
+    }
+
+    private void startRandomAnimation() {
+        Thread animationThread = new Thread(() -> {
+            while (true) {
+                try {
+                    moveCharactersRandomly();
+                    Thread.sleep(100 + random.nextInt(200)); // Randomize speed slightly
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        animationThread.start();
+    }
+
+    private void moveCharactersRandomly() {
+        int moveAmount = random.nextInt(6) + 2; // Move between 2-7 pixels per step
+        boolean moveLeft = random.nextBoolean(); // Randomly decide direction
+
+        if (moveLeft) {
+            leftPlayer.translate(-moveAmount, 0);
+            rightPlayer.translate(moveAmount, 0);
+        } else {
+            leftPlayer.translate(moveAmount, 0);
+            rightPlayer.translate(-moveAmount, 0);
+        }
+
+        // Constrain movement within a reasonable range
+        int leftMinX = 60, leftMaxX = 120;
+        int rightMinX = 500, rightMaxX = 560;
+
+        if (leftPlayer.getX() < leftMinX) {
+            leftPlayer.translate(moveAmount, 0);
+        } else if (leftPlayer.getX() > leftMaxX) {
+            leftPlayer.translate(-moveAmount, 0);
+        }
+
+        if (rightPlayer.getX() < rightMinX) {
+            rightPlayer.translate(moveAmount, 0);
+        } else if (rightPlayer.getX() > rightMaxX) {
+            rightPlayer.translate(-moveAmount, 0);
+        }
     }
 }
